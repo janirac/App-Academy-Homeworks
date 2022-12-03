12f8392ac3b7d302a5d008894212a8f5d9007845
Janira Crispin <janiracrispin@gmail.com>
2022-12-03 18:36:09 -0500

    exercise 3 basic map

--- W4D1_HW/diy_adts.rb
+++ W4D1_HW/diy_adts.rb
@@ -36,3 +36,43 @@ class Queue
         @people.first
     end 
 end 
+
+# Exercise 3 Map
+class Map
+    def initialize
+        @underlying_array = []
+    end 
+
+    def set(key, value)
+        @underlying_array.each_with_index do |pairs, idx|
+            if pairs[0] == key
+               return @underlying_array[idx] = [key, value]
+            end 
+        end 
+
+        @underlying_array << [key, value]
+    end 
+    
+    def get(key)
+        @underlying_array.each do |pairs|
+            if pairs[0] == key
+                return pairs[1]
+            end 
+        end 
+        nil
+    end 
+
+    def delete(key)
+        value = get(key)
+        @underlying_array.reject! { |pairs| pairs[0] == key}
+        value
+    end
+
+    def show(value)
+        deep_dup(@underlying_array)
+    end
+
+    def deep_dup(arr)
+        arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+    end
+end 
